5159c58035ac13cd54f22800f5285e11da8256df
chiheb <chiheb.bbrahim@gmail.com>
2018-09-10 23:37:34 +0200

    second

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost/hamadi")
-  .then(() => console.log("cezfefefzezezected"))
+  .then(() => console.log("connected"))
   .catch(err => console.log(err));
-const bookSchema = new mongoose.Schema({
-  name: String,
-  author: String,
-  tags: [String],
-  date: { type: Date, default: Date.now },
-  isPublished: Boolean
+const personSchema = new mongoose.Schema({
+  lastName: String,
+  firstName: String,
+  email: String,
+  age: Number
 });
-const Book = mongoose.model("book", bookSchema);
-const Book1 = mongoose.model("book1", bookSchema);
-async function createBook() {
-  const book = new Book({
-    name: "the prince of milk",
-    author: "exurb1a",
-    tags: ["sci-fi", "spacy-shit"],
-    isPublished: true
+const Person = mongoose.model("contact-list", personSchema);
+async function addPerson(param) {
+  const person = new Person({
+    ...param
   });
-  const result = await book.save();
+  const result = await person.save();
   console.log(result);
 }
+
 async function getBooks() {
   const books = await Book.find().sort({ date: -1 });
   console.log(books);
 }
-
-getBooks();
+const entree1 = { lastName: "ben lahmer", firstName: "fares", email: "fares@gmail.com", age: 26 };
+const entree2 = { lastName: "kefi", firstName: "seif", email: "kefi@gmail.com", age: 15 };
+const entree3 = { lastName: "fatnassi", firstName: "sarra", email: "sarra@gmail.com", age: 40 };
+const entree4 = { lastName: "ben yahia", firstName: "rym", age: 4 };
+const entree5 = { lastName: "cherif", firstName: "sami", age: 3 };
+addPerson(entree1)
+addPerson(entree2)
+addPerson(entree3)
+addPerson(entree4)
+addPerson(entree5)
