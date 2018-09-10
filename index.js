f3bde5bcbd54e4bf3ed79f50cefbbea8b76ad4b9
chiheb <chiheb.bbrahim@gmail.com>
2018-09-10 23:46:22 +0200

    pretty

--- index.js
+++ index.js
@@ -22,13 +22,28 @@ async function getBooks() {
   const books = await Book.find().sort({ date: -1 });
   console.log(books);
 }
-const entree1 = { lastName: "ben lahmer", firstName: "fares", email: "fares@gmail.com", age: 26 };
-const entree2 = { lastName: "kefi", firstName: "seif", email: "kefi@gmail.com", age: 15 };
-const entree3 = { lastName: "fatnassi", firstName: "sarra", email: "sarra@gmail.com", age: 40 };
+const entree1 = {
+  lastName: "ben lahmer",
+  firstName: "fares",
+  email: "fares@gmail.com",
+  age: 26
+};
+const entree2 = {
+  lastName: "kefi",
+  firstName: "seif",
+  email: "kefi@gmail.com",
+  age: 15
+};
+const entree3 = {
+  lastName: "fatnassi",
+  firstName: "sarra",
+  email: "sarra@gmail.com",
+  age: 40
+};
 const entree4 = { lastName: "ben yahia", firstName: "rym", age: 4 };
 const entree5 = { lastName: "cherif", firstName: "sami", age: 3 };
-addPerson(entree1)
-addPerson(entree2)
-addPerson(entree3)
-addPerson(entree4)
-addPerson(entree5)
+addPerson(entree1);
+addPerson(entree2);
+addPerson(entree3);
+addPerson(entree4);
+addPerson(entree5);
